7ffcdd0e8b9f73e7e2afd8e4bf2914be14b06d64
Ujjawal Tyagi <ujjawaltyagi858@gmail.com>
2023-12-24 19:49:10 +0530

    hello

--- src/PlaylistTransferButton.jsx
+++ src/PlaylistTransferButton.jsx
@@ -2,54 +2,38 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const PlaylistTransferButton = () => {
-    const spotifyPlaylistName = localStorage.getItem('selectedPlaylistName');
-  const handleTransfer = async () => {
-    try {
-      // Get the stored Spotify playlist ID from local storage
-      const spotifyPlaylistName = localStorage.getItem('selectedPlaylistName');
-
-      if (!spotifyPlaylistName) {
-        console.error('Spotify playlist ID not found in local storage');
-        return;
-      }
-
-      // Make a request to your backend to initiate the transfer
-      const response = await axios.post(`http://localhost:5005/transfer-to-youtube-music`,{
-        playlistName:{spotifyPlaylistName},
-      });
-
-     
-
-      console.log('Transfer Response:', response.data);
-
-      // Optional: Handle success or show a notification
-    } catch (error) {
-      console.error('Error transferring playlist:', error.message);
-      // Optional: Handle errors or show an error notification
-    }
-  };
-
+    let spotifyPlaylistName = localStorage.getItem('selectedPlaylistName');
   
-  const temporaryhandleTransfer=async()=>{
 
-      await axios.post(`http://localhost:5005/temp-playlist-create`,{
-        pName:spotifyPlaylistName,
-      })
-  .then(response => {
-    console.log('done with playlist transfer');
+let songarray;
+let PlaylistObject = localStorage.getItem('selectedPlaylistDetails');
+let spotifyPlaylist = JSON.parse(PlaylistObject);
+//console.log(spotifyPlaylist)
+if (spotifyPlaylist && spotifyPlaylist.tracks) {
+  songarray = spotifyPlaylist.tracks;
+}
+//console.log(songarray);
+const temporaryHandleTransfer = async () => {
+  try {
+    const response = await axios.post('http://localhost:5005/temp-playlist-create', {
+      pName: spotifyPlaylistName, // Make sure spotifyPlaylistName is defined
+      songarray: songarray,
+    });
+
+    console.log('Done with playlist transfer');
     // Handle the response data if needed
     console.log(response.data);
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Error:', error.message);
-    // Handle the error
-  });
+    // Handle the error more specifically if needed
+  }
+};
+
 
     
-  }
   return (
     <div>
-      <button onClick={temporaryhandleTransfer}>Transfer Playlist to YouTube Music</button>
+      <button onClick={temporaryHandleTransfer}>Transfer Playlist to YouTube Music</button>
     </div>
   );
 };
